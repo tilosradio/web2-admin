a3252b51f44802e87087d22f7f5bea573d5b20d6
Elek, Márton <level2@anix.net>
2014-12-04 22:08:42 +0100

    new(stat): monthly stat is parameterized

--- app/js/controllers/Stat.js
+++ app/js/controllers/Stat.js
@@ -7,8 +7,12 @@ angular.module('tilosAdmin').config(function ($routeProvider) {
   });
 });
 
-angular.module('tilosAdmin').controller('StatListenerCtrl', function ($scope, $http, API_SERVER_ENDPOINT) {
-  $http.get(API_SERVER_ENDPOINT + "/api/v1/stat/listener").success(function (data) {
+angular.module('tilosAdmin').controller('StatListenerCtrl', function ($scope, $http, API_SERVER_ENDPOINT, $location) {
+  var query = "";
+  if (($location.search()).from && ($location.search()).to) {
+    query = "?from=" + ($location.search()).from + "&to=" + ($location.search()).to;
+  }
+  $http.get(API_SERVER_ENDPOINT + "/api/v1/stat/listener" + query).success(function (data) {
     $scope.listeners = data;
   });
 });
