919f44424133f9cecb566ccb848b27d39601c013
Elek, Márton <level2@anix.net>
2015-06-03 22:00:37 +0200

    simple user list page

--- app/js/controllers/User.js
+++ app/js/controllers/User.js
@@ -12,41 +12,54 @@ angular.module('tilosAdmin').config(function ($routeProvider) {
       data: function ($route, Users) {
         return Users.get({id: $route.current.params.id});
       },
-    }});
+    }
+  });
+  $routeProvider.when('/userlist', {
+    templateUrl: 'views/userlist.html',
+    controller: 'UserListCtrl'
+  });
 });
 
 
 angular.module('tilosAdmin')
-    .controller('UserEditCtrl', function ($location, $scope, $routeParams, API_SERVER_ENDPOINT, $http, $cacheFactory, data, $rootScope) {
-      $scope.user = data;
-      $scope.save = function () {
-        $http.put(API_SERVER_ENDPOINT + '/api/v0/user/' + $routeParams.id, $scope.user).success(function (data) {
-          var httpCache = $cacheFactory.get('$http');
-          httpCache.remove(API_SERVER_ENDPOINT + '/api/v0/user/' + $scope.user.id);
-          $http.get(API_SERVER_ENDPOINT + '/api/v0/user/me').success(function (data) {
-            $rootScope.user = data;
-            $location.path('/user/' + $scope.user.id);
-          });
-          $scope.error = "";
-        }).error(function (data) {
-          if (data.error) {
-            $scope.error = data.error;
-          } else {
-            $scope.error = "Unknown server error";
-          }
-        });
+  .controller('UserListCtrl', function ($scope, $http, API_SERVER_ENDPOINT) {
+    $http.get(API_SERVER_ENDPOINT + '/api/int/user').success(function (data) {
+      $scope.users = data
+    })
 
-      }
+  });
+
+angular.module('tilosAdmin')
+  .controller('UserEditCtrl', function ($location, $scope, $routeParams, API_SERVER_ENDPOINT, $http, $cacheFactory, data, $rootScope) {
+    $scope.user = data;
+    $scope.save = function () {
+      $http.put(API_SERVER_ENDPOINT + '/api/v0/user/' + $routeParams.id, $scope.user).success(function (data) {
+        var httpCache = $cacheFactory.get('$http');
+        httpCache.remove(API_SERVER_ENDPOINT + '/api/v0/user/' + $scope.user.id);
+        $http.get(API_SERVER_ENDPOINT + '/api/v0/user/me').success(function (data) {
+          $rootScope.user = data;
+          $location.path('/user/' + $scope.user.id);
+        });
+        $scope.error = "";
+      }).error(function (data) {
+        if (data.error) {
+          $scope.error = data.error;
+        } else {
+          $scope.error = "Unknown server error";
+        }
+      });
+
     }
+  }
 )
 ;
 
 angular.module('tilosAdmin')
-    .controller('UserCtrl', function ($scope) {
-    });
+  .controller('UserCtrl', function ($scope) {
+  });
 
 angular.module('tilosAdmin').factory('Users', ['API_SERVER_ENDPOINT', '$resource', function (server, $resource) {
   return $resource(server + '/api/v0/user/:id', null, {
-    'update': { method: 'PUT'}
+    'update': {method: 'PUT'}
   });
 }]);
