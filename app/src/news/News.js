5c3efd924c63abe9987ed7dc541056aa1be3e692
Elek, Márton <elek@users.noreply.github.com>
2016-02-07 22:50:53 +0100

    new: tech view of the news

--- app/src/news/News.js
+++ app/src/news/News.js
@@ -6,10 +6,20 @@ angular.module('tilosAdmin').config(function ($stateProvider) {
     templateUrl: 'news/news.html',
     controller: 'NewsCtrl'
   });
+  $stateProvider.state('news-dayview-today', {
+    url: '/news/day',
+    templateUrl: 'news/news-today.html',
+    controller: 'NewsTodayCtrl'
+  });
   $stateProvider.state('news-day', {
     url: '/news/:year/:month/:day',
     templateUrl: 'news/news.html',
     controller: 'NewsCtrl'
+  });
+  $stateProvider.state('news-dayview', {
+    url: '/news/day/:year/:month/:day',
+    templateUrl: 'news/news-today.html',
+    controller: 'NewsTodayCtrl'
   });
   $stateProvider.state('news-file', {
     url: '/news/file/:id',
@@ -114,6 +124,73 @@ angular.module('tilosAdmin').controller('NewsBlockCtrl', function ($http, API_SE
     });
   }
 });
+
+angular.module('tilosAdmin').controller('NewsTodayCtrl', function ($http, API_SERVER_ENDPOINT, $scope, $stateParams) {
+  $scope.selectedDate = new Date();
+  if ($stateParams.year) {
+    $scope.selectedDate = new Date($stateParams.year, $stateParams.month - 1, $stateParams.day, 0, 0, 0);
+  } else {
+    $scope.selectedDate.setHours(0);
+    $scope.selectedDate.setMinutes(0);
+    $scope.selectedDate.setSeconds(0);
+  }
+
+  $scope.current = $scope.selectedDate.getTime();
+
+  var zeroDate = new Date($scope.selectedDate.getTime());
+  zeroDate.setHours(5);
+  zeroDate.setMinutes(0);
+  zeroDate.setSeconds(0);
+  zeroDate.setMilliseconds(0);
+
+
+  var dateToStr = function (dt) {
+    var monthStr = ('0' + (dt.getMonth() + 1)).slice(-2);
+    var dayStr = ('0' + dt.getDate()).slice(-2);
+    var yearStr = '' + dt.getFullYear();
+    return yearStr + '-' + monthStr + '-' + dayStr
+  };
+  var dateStr = dateToStr($scope.selectedDate);
+  var loadBlocks = function () {
+    $http.get(API_SERVER_ENDPOINT + '/api/v1/news/block/' + dateStr).success(function (data) {
+      $scope.blocks = data;
+      for (var i = 0; i < data.length; i++) {
+        var offset = ($scope.blocks[i].date * 1000 - zeroDate.getTime()) / (30 * 60 * 1000);
+        var t = Math.round(offset) - offset;
+
+        var offset = Math.round(offset) - t * 3;
+
+        $scope.blocks[i].top = offset * 60;
+        $scope.blocks[i].height = ($scope.blocks[i].expectedDuration / 60 / 15 * 30) * 3;
+        if ($scope.blocks[i].name.indexOf("havolt") > 0) {
+          $scope.blocks[i].left = 700;
+        } else {
+          $scope.blocks[i].left = 270;
+        }
+      }
+    });
+  };
+  var start = $scope.selectedDate.getTime();
+  var end = start + 24 * 60 * 60 * 1000;
+  $http.get(API_SERVER_ENDPOINT + '/api/v1/episode?start=' + start + "&end=" + end).success(function (data) {
+    $scope.episodes = data;
+    $scope.episodes.forEach(function (episode) {
+      var hourOffset = (episode.plannedFrom - zeroDate.getTime()) / (1000.0 * 60 * 60 )
+
+      episode.top = hourOffset * 120;
+    });
+  });
+  $scope.hours = [];
+  for (var hour = 5; hour < 24; hour++) {
+    var h = {};
+    h.label = "" + hour + ":00";
+    h.position = (hour - 5) * 4 * 30;
+    $scope.hours.push(h);
+  }
+  loadBlocks();
+
+});
+
 angular.module('tilosAdmin').controller('NewsCtrl', function ($http, API_SERVER_ENDPOINT, $scope, $stateParams) {
     $scope.selectedDate = new Date();
     if ($stateParams.year) {
@@ -150,6 +227,7 @@ angular.module('tilosAdmin').controller('NewsCtrl', function ($http, API_SERVER_
     var loadBlocks = function () {
       $http.get(API_SERVER_ENDPOINT + '/api/v1/news/block/' + dateStr).success(function (data) {
         $scope.blocks = data;
+
       });
     }
     loadBlocks();
